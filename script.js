924f4e36a6e9d4be009278746029c172e42a1a7e
chaitanya karthik <ychaitanyakarthik@gmail.com>
2024-09-25 07:28:23 +0530

    signinform

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-
 const URL = "https://api.themoviedb.org/3/discover/movie?api_key=d92a8cd2fc953e1b600568b97180bdf9";
 const getFacts = async () => {
-    console.log("gtting data.....");
+    console.log("getting data.....");
     let response = await fetch(URL); // Fetch the data
-    console.log(response);
-    return response;
-    
+    console.log(response);// Log the actual data
+    let data = await response.json()
+    console.log(data);
+
+}
+
+
+function auth(){
+    var emailaddress = document.getElementById('emailaddress').value;
+    var password = document.getElementById('password').value;
+
+    if(emailaddress == 'admin@gmail.com' && password == 1234567)
+    {
+        window.location.assign('main.html');
+        alert("login successfully");
+    }
+    else{
+        alert("Invalid Information");
+        return;
+    }
 }
 
 
 
-// const URL = "https://api.themoviedb.org/3/discover/movie?api_key=d92a8cd2fc953e1b600568b97180bdf9";
-// const getFacts = async () => {
-//     console.log("gtting data.....");
-//     let response = await fetch(URL); // Fetch the data
-// 	 return response;
-// }
